36186262ea9adfff81f2174dcfddb69a8e7ccc05
Ryos <105155814+Kauaryos@users.noreply.github.com>
2022-06-20 14:44:08 -0300

    INSERT Projeto SA1

--- Insert db faculdade.sql
+++ Insert db faculdade.sql
@@ -4,7 +4,7 @@ INSERT INTO tipo_logradouro
     (cod_tipo_logradouro,tipo_logradouro)                 
 VALUES                                                               
     ('1','rua'),
-    ('2','chácara'),
+    ('2','chacara'),
     ('3','lote'),
     ('4','avenida'), 
     ('5','quadra'); 
@@ -39,7 +39,7 @@ VALUES
     ('2','celular'),
     ('3','comercio');
 
- INSERT INTO telefone                                                      
+INSERT INTO telefone                                                      
     (cod_telefone,numero_telefone,fk_cod_tipo_telefone)                 
 VALUES                                                               
     ('1','564456542','1'),
@@ -92,7 +92,7 @@ VALUES
     ('5','matematica','2'),
     ('6','Pscicologia','5'),
     ('7','engenharia eletrica','6'),
-    ('8','animação','5'),
+    ('8','animacao','5'),
     ('9','anatomia','3'),
     ('10','designer','5');
 
@@ -117,7 +117,7 @@ VALUES
     ('4','Diogo','Defante','547564668','1','M','Tata','Cocielo','defaante@gmail.com','994827362','6','6','4'),
     ('5','Alan','zoka','756695867','2','M','joana','joao','alan@gmail.com','646354751',NULL,NULL,'5'),
     ('6','Murilo','couto','654638478','1','M','murila','carlos','muri@gmail.com','7463672352','10','8','10'),
-    ('7','João','Pé de feijão','454563637','1','M','rafaela','pedro','jão@gmail.com','948572904','10','8','10'),
+    ('7','Joao','Pe de feijao','454563637','1','M','rafaela','pedro','jao@gmail.com','948572904','10','8','10'),
     ('8','Arthur','lanches','454563674','1','M','maria','julio','athurzin@gmail.com','5673465873','10','5','6'),
     ('9','Scooby','Doo','346565383','1','M','Salsicha',NULL,'salsicha@gmail.com','798378584','6','5','7'),
     ('10','Peter','Parker','46336347','1','M','May','Ben','miranha@gmail.com','7346565838','2','4','8'),
@@ -141,11 +141,11 @@ VALUES
     ('1','Raciocinio logico','200','coisas logicas','56','2'),
     ('2','Pscicologia','200','Cognitiva','30','4'),
     ('3','Eletronica digital','180','Eletrizante','30','4'),
-    ('4','Programação em C','200','Estudar codigo C','21','5'),
+    ('4','Programacao em C','200','Estudar codigo C','21','5'),
     ('5','rico','210','fique rico','2','2'),
     ('6','Sociologia','120','adorar umas plantas ai','60','3'),
-    ('7','preguiça','400','não fazer insert','5','5'),
-    ('8','fuchico','200','fuchiqueiro de plantão','18','5'),
+    ('7','preguica','400','nao fazer insert','5','5'),
+    ('8','fuchico','200','fuchiqueiro de plantao','18','5'),
     ('9','jogos','150','criar jogos','23','5'),
     ('10','pexeiro','150','mentir historias','30','5'),
     ('11','Contabilidade','140','Estudar matematica','17','2'),
@@ -164,7 +164,7 @@ VALUES
     ('24','PHP','500','topzeira','25','6'),
     ('25','Presidente','111','Estudar as pedras','35','1'),
     ('26','cantor','100','voz boa','13','5'),
-    ('27','DBA','293','Não fazer insert','200','6'),
+    ('27','DBA','293','Nao fazer insert','200','6'),
     ('28','html','200','fazer sites','25','6'),
     ('29','Portugues','200','ler hqs','29','6'),
     ('30','final','90','terminar as coisas','37','6');
@@ -174,7 +174,7 @@ INSERT INTO professor
     (cod_professor,nome_professor, sobrenome_professor, status, fk_cod_departamento)                 
 VALUES                                                               
     ('1','Mestre','kame','1','2'),
-    ('2','mestre','ancião','1','1'),
+    ('2','mestre','anciao','1','1'),
     ('3','Raimundo','nonato','1','5'),
     ('4','professor','girafales','1','4'),
     ('5','luciano','lopes','1','1'),
