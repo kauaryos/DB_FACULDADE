87895e3e45d862d7da0e959fab3b676a8dc85f8f
Ryos <105155814+Kauaryos@users.noreply.github.com>
2022-06-22 11:22:55 -0300

    mysqldump Backup

--- backup22062022.sql
+++ backup22062022.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 8.0.29, for Linux (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.29, for Win64 (x86_64)
 --
--- Host: localhost    Database: faculdade
+-- Host: localhost    Database: db_faculdade
 -- ------------------------------------------------------
--- Server version	8.0.29-0ubuntu0.20.04.3
+-- Server version	8.0.29
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -52,7 +52,7 @@ CREATE TABLE `aluno` (
 
 LOCK TABLES `aluno` WRITE;
 /*!40000 ALTER TABLE `aluno` DISABLE KEYS */;
-INSERT INTO `aluno` VALUES (1,'Goku','Sayajin',956473875,'7665534142',1,'M','Bardock','Akira','goku@gmail.com',1,4,1),(2,'Bruce','wayne',968658548,'4646723234',2,'M','Bruce','Marta','batman@gmail.com',NULL,NULL,2),(3,'Ben','Tenyson',1010101010,'5733874473',1,'M','Juca','Ana','ben@gmail.com',1,5,3),(4,'Diogo','Defante',547564668,'994827362',1,'M','Cocielo','Tata','defaante@gmail.com',6,6,4),(5,'Alan','zoka',756695867,'646354751',2,'M','joao','joana','alan@gmail.com',NULL,NULL,5),(6,'Murilo','couto',654638478,'7463672352',1,'M','carlos','murila','muri@gmail.com',10,8,10),(7,'João','Pé de feijão',454563637,'948572904',1,'M','pedro','rafaela','jão@gmail.com',10,8,10),(8,'Arthur','lanches',454563674,'5673465873',1,'M','julio','maria','athurzin@gmail.com',10,5,6),(9,'Scooby','Doo',346565383,'798378584',1,'M',NULL,'Salsicha','salsicha@gmail.com',6,5,7),(10,'Peter','Parker',46336347,'7346565838',1,'M','Ben','May','miranha@gmail.com',2,4,8),(11,'Patrick','Estrela',64653637,'456474574',1,'M','Bob','Joana','estrelinha@gmail.com',10,8,9),(12,'Roberto','Bolanos',65764363,'121212121',1,'M','Bruno','Barbara','robertin@gmail.com',6,2,10),(13,'Rodrigo','Faro',67474567,'6787656765',1,'M','casemiro','elen','namoro@gmail.com',3,4,11),(14,'Ricardo','Cruz',46464657,'778345345',1,'M','joao','gabriela','ricardo@gmail.com',3,4,12),(15,'James','Bond',747574574,'9898565675',1,'M','mario','carol','james@gmail.com',3,8,13),(16,'Chico','Buarque',53637375,'789987897',2,'M','Chico','lorena','buarque@gmail.com',NULL,NULL,14),(17,'Ramon','Madruga',46478383,'680086806',1,'M','felipe','valdirene','madruga@gmail.com',1,7,15),(18,'Rick','Sanches',46525284,'8677462181',1,'M','joao','renata','ricka@gmail.com',1,7,16),(19,'Zack','Snyder',63466376,'343534534',1,'M','clarck','diana','superman@gmail.com',6,6,17),(20,'Adam','Sandler',36363673,'1574353453',1,'M','Lucas','Divina','AMo@gmail.com',8,8,18);
+INSERT INTO `aluno` VALUES (1,'Goku','Sayajin',956473875,'7665534142',1,'M','Bardock','Akira','goku@gmail.com',1,4,1),(2,'Bruce','wayne',968658548,'4646723234',2,'M','Bruce','Marta','batman@gmail.com',NULL,NULL,2),(3,'Ben','Tenyson',1010101010,'5733874473',1,'M','Juca','Ana','ben@gmail.com',1,5,3),(4,'Diogo','Defante',547564668,'994827362',1,'M','Cocielo','Tata','defaante@gmail.com',6,6,4),(5,'Alan','zoka',756695867,'646354751',2,'M','joao','joana','alan@gmail.com',NULL,NULL,5),(6,'Murilo','couto',654638478,'7463672352',1,'M','carlos','murila','muri@gmail.com',10,8,10),(7,'Joao','Pe de feijao',454563637,'948572904',1,'M','pedro','rafaela','jao@gmail.com',10,8,10),(8,'Arthur','lanches',454563674,'5673465873',1,'M','julio','maria','athurzin@gmail.com',10,5,6),(9,'Scooby','Doo',346565383,'798378584',1,'M',NULL,'Salsicha','salsicha@gmail.com',6,5,7),(10,'Peter','Parker',46336347,'7346565838',1,'M','Ben','May','miranha@gmail.com',2,4,8),(11,'Patrick','Estrela',64653637,'456474574',1,'M','Bob','Joana','estrelinha@gmail.com',10,8,9),(12,'Roberto','Bolanos',65764363,'121212121',1,'M','Bruno','Barbara','robertin@gmail.com',6,2,10),(13,'Rodrigo','Faro',67474567,'6787656765',1,'M','casemiro','elen','namoro@gmail.com',3,4,11),(14,'Ricardo','Cruz',46464657,'778345345',1,'M','joao','gabriela','ricardo@gmail.com',3,4,12),(15,'James','Bond',747574574,'9898565675',1,'M','mario','carol','james@gmail.com',3,8,13),(16,'Chico','Buarque',53637375,'789987897',2,'M','Chico','lorena','buarque@gmail.com',NULL,NULL,14),(17,'Ramon','Madruga',46478383,'680086806',1,'M','felipe','valdirene','madruga@gmail.com',1,7,15),(18,'Rick','Sanches',46525284,'8677462181',1,'M','joao','renata','ricka@gmail.com',1,7,16),(19,'Zack','Snyder',63466376,'343534534',1,'M','clarck','diana','superman@gmail.com',6,6,17),(20,'Adam','Sandler',36363673,'1574353453',1,'M','Lucas','Divina','AMo@gmail.com',8,8,18);
 /*!40000 ALTER TABLE `aluno` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -106,7 +106,7 @@ CREATE TABLE `curso` (
 
 LOCK TABLES `curso` WRITE;
 /*!40000 ALTER TABLE `curso` DISABLE KEYS */;
-INSERT INTO `curso` VALUES (1,6,'Engenharia software'),(2,6,'analista de sistema'),(3,4,'Biologia'),(4,5,'historia'),(5,2,'matematica'),(6,5,'Pscicologia'),(7,6,'engenharia eletrica'),(8,5,'animação'),(9,3,'anatomia'),(10,5,'designer');
+INSERT INTO `curso` VALUES (1,6,'Engenharia software'),(2,6,'analista de sistema'),(3,4,'Biologia'),(4,5,'historia'),(5,2,'matematica'),(6,5,'Pscicologia'),(7,6,'engenharia eletrica'),(8,5,'animacao'),(9,3,'anatomia'),(10,5,'designer');
 /*!40000 ALTER TABLE `curso` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -133,7 +133,7 @@ CREATE TABLE `curso_disciplina` (
 
 LOCK TABLES `curso_disciplina` WRITE;
 /*!40000 ALTER TABLE `curso_disciplina` DISABLE KEYS */;
-INSERT INTO `curso_disciplina` VALUES (1,1),(2,2),(3,3),(4,4),(5,5),(6,6),(7,7),(8,8),(9,9),(10,10);
+INSERT INTO `curso_disciplina` VALUES (1,1),(2,2),(3,3),(4,4),(5,5),(6,6),(7,7),(8,8),(9,9),(10,10),(1,11),(2,12),(3,13),(4,14),(5,15),(6,16),(7,17),(8,18),(9,19),(10,20),(1,21),(2,22),(3,23),(4,24),(5,25),(6,26),(7,27),(8,28),(9,29),(10,30);
 /*!40000 ALTER TABLE `curso_disciplina` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -187,7 +187,7 @@ CREATE TABLE `disciplina` (
 
 LOCK TABLES `disciplina` WRITE;
 /*!40000 ALTER TABLE `disciplina` DISABLE KEYS */;
-INSERT INTO `disciplina` VALUES (1,'Raciocinio logico',200,'coisas logicas',56,2),(2,'Pscicologia',200,'Cognitiva',30,4),(3,'Eletronica digital',180,'Eletrizante',30,4),(4,'Programação em C',200,'Estudar codigo C',21,5),(5,'rico',210,'fique rico',2,2),(6,'Sociologia',120,'adorar umas plantas ai',60,3),(7,'preguiça',400,'não fazer insert',5,5),(8,'fuchico',200,'fuchiqueiro de plantão',18,5),(9,'jogos',150,'criar jogos',23,5),(10,'pexeiro',150,'mentir historias',30,5),(11,'Contabilidade',140,'Estudar matematica',17,2),(12,'minion',20,'banana',10,5),(13,'desistente',100,'desistir',56,5),(14,'Biologia',225,'virar professor aos 20',15,4),(15,'construtor',50,'contruir',50,5),(16,'turista',200,'faltador de aula',30,5),(17,'jogador caro',135,'pegar muie',100,5),(18,'twitero',170,'falar besteirol',50,6),(19,'lolzeiro',220,'saia do prata',12,6),(20,'Roubo',99,'precisa ser presidente',9,3),(21,'minecraft',100,'jogar',100,6),(22,'editor',90,'editar coisas',20,6),(23,'Java',175,'fazer javas',32,6),(24,'PHP',500,'topzeira',25,6),(25,'Presidente',111,'Estudar as pedras',35,1),(26,'cantor',100,'voz boa',13,5),(27,'DBA',293,'Não fazer insert',200,6),(28,'html',200,'fazer sites',25,6),(29,'Portugues',200,'ler hqs',29,6),(30,'final',90,'terminar as coisas',37,6);
+INSERT INTO `disciplina` VALUES (1,'Raciocinio logico',200,'coisas logicas',56,2),(2,'Pscicologia',200,'Cognitiva',30,4),(3,'Eletronica digital',180,'Eletrizante',30,4),(4,'Programacao em C',200,'Estudar codigo C',21,5),(5,'rico',210,'fique rico',2,2),(6,'Sociologia',120,'adorar umas plantas ai',60,3),(7,'preguica',400,'nao fazer insert',5,5),(8,'fuchico',200,'fuchiqueiro de plantao',18,5),(9,'jogos',150,'criar jogos',23,5),(10,'pexeiro',150,'mentir historias',30,5),(11,'Contabilidade',140,'Estudar matematica',17,2),(12,'minion',20,'banana',10,5),(13,'desistente',100,'desistir',56,5),(14,'Biologia',225,'virar professor aos 20',15,4),(15,'construtor',50,'contruir',50,5),(16,'turista',200,'faltador de aula',30,5),(17,'jogador caro',135,'pegar muie',100,5),(18,'twitero',170,'falar besteirol',50,6),(19,'lolzeiro',220,'saia do prata',12,6),(20,'Roubo',99,'precisa ser presidente',9,3),(21,'minecraft',100,'jogar',100,6),(22,'editor',90,'editar coisas',20,6),(23,'Java',175,'fazer javas',32,6),(24,'PHP',500,'topzeira',25,6),(25,'Presidente',111,'Estudar as pedras',35,1),(26,'cantor',100,'voz boa',13,5),(27,'DBA',293,'Nao fazer insert',200,6),(28,'html',200,'fazer sites',25,6),(29,'Portugues',200,'ler hqs',29,6),(30,'final',90,'terminar as coisas',37,6);
 /*!40000 ALTER TABLE `disciplina` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -216,7 +216,7 @@ CREATE TABLE `disciplina_historico` (
 
 LOCK TABLES `disciplina_historico` WRITE;
 /*!40000 ALTER TABLE `disciplina_historico` DISABLE KEYS */;
-INSERT INTO `disciplina_historico` VALUES (10.00,20,1,1),(5.00,19,2,2),(5.00,18,3,3),(1.00,17,4,4),(6.00,16,5,5),(5.00,15,6,6),(5.00,14,7,7),(5.00,13,8,8),(5.00,12,9,9),(5.00,11,10,10),(5.00,10,11,11),(10.00,9,12,12),(8.40,8,13,13),(9.10,7,14,14),(8.90,6,15,15),(9.90,5,16,16),(10.00,4,17,17),(5.40,3,18,18),(6.80,2,19,19),(7.90,1,20,20);
+INSERT INTO `disciplina_historico` VALUES (10.00,20,1,1),(5.00,18,3,3),(1.00,17,4,4),(5.00,15,6,6),(5.00,14,7,7),(5.00,13,8,8),(5.00,12,9,9),(5.00,11,10,10),(5.00,10,11,11),(10.00,9,12,12),(8.40,8,13,13),(9.10,7,14,14),(8.90,6,15,15),(10.00,4,17,17),(5.40,3,18,18),(6.80,2,19,19),(7.90,1,20,20);
 /*!40000 ALTER TABLE `disciplina_historico` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -274,7 +274,7 @@ CREATE TABLE `historico` (
 
 LOCK TABLES `historico` WRITE;
 /*!40000 ALTER TABLE `historico` DISABLE KEYS */;
-INSERT INTO `historico` VALUES (1,'2021-06-01','2022-11-10',1),(2,'2021-05-02','2022-02-11',2),(3,'2021-07-03','2022-03-12',3),(4,'2021-08-04','2022-04-13',4),(5,'2021-09-05','2022-05-14',5),(6,'2021-09-06','2022-06-15',6),(7,'2021-09-07','2022-08-16',7),(8,'2021-07-08','2022-05-17',8),(9,'2021-06-09','2022-08-18',9),(10,'2021-02-10','2022-12-19',10),(11,'2021-05-11','2022-03-20',11),(12,'2021-03-12','2022-12-30',12),(13,'2021-07-13','2022-03-13',13),(14,'2021-08-14','2022-09-18',14),(15,'2021-02-15','2022-03-25',15),(16,'2021-07-16','2022-07-23',16),(17,'2021-12-17','2022-09-22',17),(18,'2021-11-18','2022-01-17',18),(19,'2021-11-19','2022-02-24',19),(20,'2021-12-20','2022-07-29',20);
+INSERT INTO `historico` VALUES (1,'2021-06-01','2022-11-10',1),(3,'2021-07-03','2022-03-12',3),(4,'2021-08-04','2022-04-13',4),(6,'2021-09-06','2022-06-15',6),(7,'2021-09-07','2022-08-16',7),(8,'2021-07-08','2022-05-17',8),(9,'2021-06-09','2022-08-18',9),(10,'2021-02-10','2022-12-19',10),(11,'2021-05-11','2022-03-20',11),(12,'2021-03-12','2022-12-30',12),(13,'2021-07-13','2022-03-13',13),(14,'2021-08-14','2022-09-18',14),(15,'2021-02-15','2022-03-25',15),(17,'2021-12-17','2022-09-22',17),(18,'2021-11-18','2022-01-17',18),(19,'2021-11-19','2022-02-24',19),(20,'2021-12-20','2022-07-29',20);
 /*!40000 ALTER TABLE `historico` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -303,7 +303,7 @@ CREATE TABLE `professor` (
 
 LOCK TABLES `professor` WRITE;
 /*!40000 ALTER TABLE `professor` DISABLE KEYS */;
-INSERT INTO `professor` VALUES (1,2,'Mestre','kame',1),(2,1,'mestre','ancião',1),(3,5,'Raimundo','nonato',1),(4,4,'professor','girafales',1),(5,1,'luciano','lopes',1),(6,2,'professor','carvalho',1),(7,5,'Kakashi','Hatake',1),(8,3,'gojo','satoru',0),(9,4,'cleiton','rasta',0),(10,5,'ribamar','vasco',0);
+INSERT INTO `professor` VALUES (1,2,'Mestre','kame',1),(2,1,'mestre','anciao',1),(3,5,'Raimundo','nonato',1),(4,4,'professor','girafales',1),(5,1,'luciano','lopes',1),(6,2,'professor','carvalho',1),(7,5,'Kakashi','Hatake',1),(8,3,'gojo','satoru',0),(9,4,'cleiton','rasta',0),(10,5,'ribamar','vasco',0);
 /*!40000 ALTER TABLE `professor` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -410,7 +410,7 @@ CREATE TABLE `tipo_logradouro` (
 
 LOCK TABLES `tipo_logradouro` WRITE;
 /*!40000 ALTER TABLE `tipo_logradouro` DISABLE KEYS */;
-INSERT INTO `tipo_logradouro` VALUES (1,'rua'),(2,'chácara'),(3,'lote'),(4,'avenida'),(5,'quadra');
+INSERT INTO `tipo_logradouro` VALUES (1,'rua'),(2,'chacara'),(3,'lote'),(4,'avenida'),(5,'quadra');
 /*!40000 ALTER TABLE `tipo_logradouro` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -477,4 +477,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-06-18 22:57:56
+-- Dump completed on 2022-06-22 11:20:54
